3b1dea7f253a13825db1d57c09971e3e4d884a45
p1xelmind <trubichkinek@gmail.com>
2025-08-11 21:20:49 +1000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tower_http::services::ServeDir;
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main(){
+    let static_service = ServeDir::new("static");
+    let static_service = static_service.append_index_html_on_directories(true);
 }
